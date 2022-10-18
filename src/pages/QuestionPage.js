aa37418864f6402b0cb22ea45e0af67c59796877
willianchu <willianchu@hotmail.com>
2022-10-18 13:25:43 -0700

    linked params to data CONS retrieve all questions

--- src/pages/QuestionPage.js
+++ src/pages/QuestionPage.js
@@ -1,9 +1,17 @@
-
+import { useParams } from "react-router-dom";
+import questionnaires from "../data/Questionnaires-Content";
 const QuestionPage = () => {
-
+  const { questionnaireId } = useParams();
+  const questionnaire = questionnaires.find(question => question.name === questionnaireId);
   return (
     <div>
-      Question
+      <h1>{questionnaire.title}</h1>
+      {questionnaire.content.map((question, index) => (
+        <div key={index}>
+          <p>{question}</p>
+          
+        </div>
+      ))}
     </div>
   );
 };
