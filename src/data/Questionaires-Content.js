5dda3849a0029b8e450c43f86f6b869bb30db3c1
willianchu <willianchu@hotmail.com>
2022-10-18 13:06:57 -0700

    generate mock data

---
+++ src/data/Questionaires-Content.js
@@ -0,0 +1,35 @@
+const questionnaires = [
+  {
+      name: 'disc-test',
+      title: 'Behavioral Test Questionnaire',
+      content: [
+          'behaviour questions 1', 
+          'behaviour questions 2',
+          'behaviour questions 3',
+          'behaviour questions 4',
+          'behaviour questions 5',
+      ]
+  },    {
+      name: 'soft-skills',
+      title: 'soft skills Questionnaire',
+      content: [
+          'questions 1',
+          'questions 2',
+          'questions 3',
+          'questions 4',
+          'questions 5',
+      ]
+  },     {
+      name: 'knowledge',
+      title: 'knowledge Questionnaire',
+      content: [
+          'questions 1',
+          'questions 2',
+          'questions 3',
+          'questions 4',
+          'questions 5',
+      ]
+  },  
+];
+
+export default questionnaires;
