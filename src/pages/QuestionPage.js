f4ac249e650f032d251a7ad72c76c34fd4352a69
willianchu <willianchu@hotmail.com>
2022-10-20 20:33:55 -0700

    set hooks to track data

--- src/pages/QuestionPage.js
+++ src/pages/QuestionPage.js
@@ -1,8 +1,15 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import questionnaires from "../data/Questionnaires-Content";
 import NotFoundPage from "./NotFoundPage";
 
 const QuestionPage = () => {
+  const [answer, setAnswer] = useState({index: 0, answers: []});
+
+  useEffect(() => {
+    setAnswer({index: 10, answers: []});
+  }, []);
+  
   const { questionnaireId } = useParams();
   const questionnaire = questionnaires.find(question => question.name === questionnaireId);
   
@@ -13,6 +20,8 @@ const QuestionPage = () => {
   return (
     <div>
       <h1>{questionnaire.title}</h1>
+      <p>This index is {answer.index} !!. </p>
+      <p>This answer is {answer.answers[0]} !!. </p>
       {questionnaire.content.map((question, index) => (
         <div key={index}>
           <p>{question}</p>
